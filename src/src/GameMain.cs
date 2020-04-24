6efb47d54d04637f76bb33493dbcc4398def9b0b
Edward Allen <crossbow711@gmail.com>
2020-04-24 15:21:18 +1000

    Rename MyGame namespace to Battleships

--- src/src/GameMain.cs
+++ src/src/GameMain.cs
@@ -1,9 +1,9 @@
 using System;
 using SwinGameSDK;
-using static SwinGameSDK.SwinGame; // requires mcs version 4+, 
-// using SwinGameSDK.SwinGame; // requires mcs version 4+, 
+using static SwinGameSDK.SwinGame; // requires mcs version 4+,
+// using SwinGameSDK.SwinGame; // requires mcs version 4+,
 
-namespace MyGame
+namespace Battleships
 {
     public class GameMain
     {
@@ -12,17 +12,17 @@ namespace MyGame
             //Open the game window
             OpenGraphicsWindow("GameMain", 800, 600);
             ShowSwinGameSplashScreen();
-            
+
             //Run the game loop
             while(false == WindowCloseRequested())
             {
                 //Fetch the next batch of UI interaction
                 ProcessEvents();
-                
+
                 //Clear the screen and draw the framerate
                 ClearScreen(Color.White);
                 DrawFramerate(0,0);
-                
+
                 //Draw onto the screen
                 RefreshScreen(60);
             }
